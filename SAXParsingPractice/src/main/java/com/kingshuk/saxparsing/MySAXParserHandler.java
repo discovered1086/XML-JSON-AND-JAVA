1a8bb2a9a60361d46a2d6aa4090886cf159ec5ab
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-01-30 13:39:42 -0600

    Adds the SAX parsing classes

--- SAXParsingPractice/src/main/java/com/kingshuk/saxparsing/MySAXParserHandler.java
+++ SAXParsingPractice/src/main/java/com/kingshuk/saxparsing/MySAXParserHandler.java
@@ -2,6 +2,7 @@ package com.kingshuk.saxparsing;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,20 +13,40 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import com.example.model.Customer;
+import com.kingshuk.saxparsing.beans.Customer;
+
+
 
 public class MySAXParserHandler extends DefaultHandler {
 
+	/**
+	 * We need to have a list here because the handler methods 
+	 * we have overriden, can't share data among each other as they are
+	 * all having void return type. We need to store the data somewhere
+	 */
 	private List<Customer> customers;
+	
+	/**
+	 * We need another variable here to keep track of
+	 * where the parser is located at any given point in time
+	 * during the parse process. We'll store the current element
+	 * in the string so that we can use that to initialize 
+	 * variables inside the customer class
+	 */
+	private String currentElement = "";
+	
+	/**
+	 * We also need a customer class here to read the data for each customer
+	 */
+	private Customer customer;
 
 	@Override
 	public void startDocument() throws SAXException {
-		System.out.println("Start document");
+		customers = new ArrayList<>();
 	}
 
 	@Override
 	public void endDocument() throws SAXException {
-		System.out.println("End document");
 	}
 
 	/*
@@ -34,17 +55,34 @@ public class MySAXParserHandler extends DefaultHandler {
 	 */
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		System.out.println("Start element: " + qName);
+		currentElement = qName;
+		
+		switch (currentElement) {
+		case "customers":			
+			break;
+		case "customer":
+			customer = new Customer();
+			
+			//Now it's time to get the ID of the customer
+			customer.setId(Integer.parseInt(attributes.getValue(Customer.ID)));
+			
+			//Now I need to add the customer to the list
+			customers.add(customer);
+			break;
+		default:
+			break;
+		}
 	}
 
 	@Override
-	public void endElement(String uri, String localName, String qName) throws SAXException {
-		System.out.println("End element: " + qName);
+	public void endElement(String uri, String localName, String qName) throws SAXException {		
+		//Now here we need to reset the value of the current element so that 
+		//we have the latest current element value at all times
+		currentElement ="";
 	}
 
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
-		System.out.println("Characters event");
 	}
 
 	public List<Customer> getCustomers(String fileName) {
--- SAXParsingPractice/src/main/java/com/kingshuk/saxparsing/MySAXParserHarness.java
+++ SAXParsingPractice/src/main/java/com/kingshuk/saxparsing/MySAXParserHarness.java
@@ -1,6 +1,9 @@
 package com.kingshuk.saxparsing;
 
+import java.util.List;
+
 import com.example.dataprovider.DataProvider;
+import com.kingshuk.saxparsing.beans.Customer;
 
 public class MySAXParserHarness {
 
@@ -11,7 +14,9 @@ public class MySAXParserHarness {
 		
 		MySAXParserHandler handler = new MySAXParserHandler();
 		
-		handler.getCustomers(xmlFile);
+		List<Customer> customers = handler.getCustomers(xmlFile);
+		
+		customers.forEach(customer -> System.out.println("Customer ID: "+customer.getId()));
 	}
 
 }
